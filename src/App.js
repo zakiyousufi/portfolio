0bb43cb7d679ba660398a030dfb6830e994e9dd2
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-05-30 14:58:40 +0430

    Add chenge them method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import './App.css';
+import ReactSwitch from "react-switch";
+import { createContext, useState } from "react";
 import HomePage from './components/Home';
+import { FiSun } from 'react-icons/fi';
+import { FaMoon } from 'react-icons/fa';
+import './App.css';
+export const ThemeContext = createContext(null);
 
-const App = () => (
-  <div className='app'>
-    <HomePage />
-  </div>
-);
+function App() {
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
+  };
+  return ( 
+    <ThemeContext.Provider value = {
+          { theme, toggleTheme } } >
+      <div className = 'app' id={theme}>
+        <div className="switch">
+          <label> {theme === "dark" ? <FaMoon /> : <FiSun />}</label>
+          <ReactSwitch 
+            onChange={toggleTheme}
+            checked={theme === "dark"}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            height={20}
+            width={40}
+            offColor="#ffae00"
+          />
+        </div>
+        <HomePage />
+      </div> 
+    </ThemeContext.Provider>
+  );
+};
 
 export default App;
