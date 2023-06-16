2003450ca8da80d110abeec943defd4950a171cd
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-06-16 20:32:02 +0430

    Create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="d-flex justify-content-end sub-nav">
+        <div className="nav-item p-3">
+          <a className="sub-info" target="_blank" rel="noreferrer" href="#home">Home</a>
+        </div>
+        <div className="nav-item p-3">
+          <a className="sub-info" target="_blank" rel="noreferrer" href="#about">About</a>
+        </div>
+        <div className="nav-item p-3">
+          <a className="sub-info" target="_blank" rel="noreferrer" href="#recommendations">Recommendations</a>
+        </div>
+        <div className="nav-item p-3">
+          <a className="sub-info" target="_blank" rel="noreferrer" href="#projects">Projects</a>
+        </div>
+        <div className="nav-item p-3">
+          <a className="sub-info" target="_blank" rel="noreferrer" href="#contact">Contact</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
