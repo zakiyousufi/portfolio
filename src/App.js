932e69a49a77c91e86291d5ac37952e76d46b011
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-06-16 20:42:00 +0430

    Import components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import ReactSwitch from "react-switch";
-import { createContext, useState } from "react";
-import HomePage from './components/Home';
+import ReactSwitch from 'react-switch';
+import { createContext, useMemo } from 'react';
 import { FiSun } from 'react-icons/fi';
 import { FaMoon } from 'react-icons/fa';
+import HomePage from './components/home/Home';
+import AboutPage from './components/about/about';
+import Recommendations from './components/recommendations/recommendations';
+import Projects from './components/projects/projects';
+import Navbar from './components/Navbar';
+import Contact from './components/contact/contact';
+import useTheme from './components/toggleTheme';
 import './App.css';
-export const ThemeContext = createContext(null);
+
+const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState("dark");
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "dark" ? "light" : "dark"));
-  };
-  return ( 
-    <ThemeContext.Provider value = {
-          { theme, toggleTheme } } >
-      <div className = 'app' id={theme}>
-        <div className="switch">
-          <label> {theme === "dark" ? <FaMoon /> : <FiSun />}</label>
-          <ReactSwitch 
+  const { theme, toggleTheme } = useTheme();
+
+  const value = useMemo(() => ({ theme, toggleTheme }), [theme, toggleTheme]);
+
+  return (
+    <ThemeContext.Provider value={value}>
+      <div
+        className="app d-flex flex-column justify-content-center align-items-center"
+        id={theme}
+      >
+        <Navbar className="navbar m-d-none" />
+        <div className="switch d-flex justify-content-center align-items-center">
+          <h4>
+            { theme === 'dark' ? <FaMoon /> : <FiSun /> }
+          </h4>
+          <ReactSwitch
             onChange={toggleTheme}
-            checked={theme === "dark"}
+            checked={theme === 'dark'}
             checkedIcon={false}
             uncheckedIcon={false}
-            height={20}
-            width={40}
+            height={30}
+            width={60}
             offColor="#ffae00"
+            className="mx-2"
           />
         </div>
-        <HomePage />
-      </div> 
+        <HomePage id="home" />
+        <AboutPage id="about" />
+        <Recommendations id="recommendations" />
+        <Projects id="projects" />
+        <Contact id="contact" />
+      </div>
     </ThemeContext.Provider>
   );
-};
+}
 
 export default App;
